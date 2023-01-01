f4e9535b6a9a42338d82181a63454ab8922abaca
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2023-01-01 20:27:53 +0530

    Added a practical1.8 for contact list simulation

---
+++ Practical1/prac1_8.pl
@@ -0,0 +1,18 @@
+/* 
+
+>====   DEFINITION   ====<
+Implement Phone list which stores name, phone no. and birthdays.
+
+>==== EXPECTED QUERY ====<
+Query to get birthday list of people whose birthdays in the current month.
+
+*/
+
+% format : contact(Person_name,contact_no,Birth_Date,Birth_Month).
+% query : contact(P,_,_,11). => Whose birthday comes in november.
+contact(henil,7874794034,25,11).
+contact(hit,1234567890,11,7).
+contact(heal,4569823107,26,12).
+contact(feni,7896523410,8,11).
+contact(alka,7567933867,13,5).
+contact(rakesh,9825546144,14,11).
